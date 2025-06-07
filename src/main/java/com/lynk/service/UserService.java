85a0d8b130141c6207f1519fbe229c4bd3060ec3
Kiran-M-P <itskiranmp@gmail.com>
2025-06-07 19:25:25 +0530

    Login API added

--- src/main/java/com/lynk/service/UserService.java
+++ src/main/java/com/lynk/service/UserService.java
@@ -1,8 +1,17 @@
 package com.lynk.service;
 
+import com.lynk.dtos.LoginRequest;
 import com.lynk.models.User;
 import com.lynk.repository.UserRepository;
+import com.lynk.security.jwt.JwtAuthenticationResponse;
+import com.lynk.security.jwt.JwtUtils;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +22,25 @@ public class UserService
 
     private PasswordEncoder passwordEncoder;
     private UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+    private JwtUtils jwtUtils;
 
     public User registerUser(User user)
     {
+        // TODO : validate email ID uniqueness
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
+
+    public JwtAuthenticationResponse authenticateUser(LoginRequest loginRequest)
+    {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String jwt = jwtUtils.generateToken(userDetails);
+        return new JwtAuthenticationResponse(jwt);
+    }
+
 }
