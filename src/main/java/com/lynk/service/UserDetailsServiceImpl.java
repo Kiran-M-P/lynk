0d57b21a92b3ead64532552fe6fcfeaec27c13ea
Kiran-M-P <itskiranmp@gmail.com>
2025-06-07 15:20:03 +0530

    Custom security config added

--- src/main/java/com/lynk/service/UserDetailsServiceImpl.java
+++ src/main/java/com/lynk/service/UserDetailsServiceImpl.java
@@ -1,16 +1,28 @@
 package com.lynk.service;
 
+import com.lynk.models.User;
+import com.lynk.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService
 {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
     {
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found for username : " + username));
+        return UserDetailsImpl.build(user);
     }
+
 }
